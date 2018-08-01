f4f8b7c277e97bf031e5373e68ccad9e42c68d30
feeohnah <fiona@studiocartogram.com>
2018-08-01 14:27:15 -0400

    mobile styles for page components

--- frontend/components/PageMast/styles.js
+++ frontend/components/PageMast/styles.js
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
 import {grid} from '../../styles/grid';
+import {breakpoints} from '../../styles/breakpoints';
 
 const StyledPageMast = styled.div`
-  grid-column: span 2;
+  grid-column: span 3;
+
+  ${breakpoints.bpM} {
+    grid-column: span 2;
+  }
 `;
 
 export {StyledPageMast};
