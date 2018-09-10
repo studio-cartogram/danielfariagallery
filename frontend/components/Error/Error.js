4c4752e535d03dbb039d8921b1e278065135b7c1
feeohnah <fiona@studiocartogram.com>
2018-09-10 20:32:05 +0200

    added error component

---
+++ frontend/components/Error/Error.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Error({type}) {
+  switch (type) {
+    case 'empty':
+      return <div>This is empty</div>;
+    default:
+      return <div>default error</div>;
+  }
+}
+
+Error.PropTypes = {
+  type: PropTypes.oneOf(['empty']),
+};
+
+export default Error;
