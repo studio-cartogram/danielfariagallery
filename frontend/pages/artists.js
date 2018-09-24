0394c75079d6a31d8f703413fef2891969acee66
feeohnah <fiona@studiocartogram.com>
2018-09-24 18:08:55 -0400

    mapping over artists to show only ones checked as being represented by DFG

--- frontend/pages/artists.js
+++ frontend/pages/artists.js
@@ -4,7 +4,9 @@ import {config} from '../config';
 import withLayout from '../decorators/withLayout';
 import Artist from '../components/Artist';
 
-const endpoint = `${config.apiUrl}/wp-json/wp/v2/artists?_embed`;
+const endpoint = `${
+  config.apiUrl
+}/wp-json/wp/v2/artists?per_page=100&_embed=true`;
 
 class ArtistIndex extends Component {
   static async getInitialProps() {
@@ -15,20 +17,25 @@ class ArtistIndex extends Component {
 
   render() {
     const artists = this.props.data;
-    const artistlistMarkup = artists.map((artist) => {
-      const image =
-        artist._embedded['wp:featuredmedia'][0].media_details.sizes.large
-          .source_url;
-      return (
-        <li key={artist.id}>
-          <Artist
-            url={`/artist/${artist.slug}`}
-            title={artist.title.rendered}
-            artistImage={image}
-          />
-        </li>
-      );
-    });
+    const artistlistMarkup = artists
+      .filter((artist) => {
+        console.log(artist);
+        return artist.acf.representation;
+      })
+      .map((artist) => {
+        const image =
+          artist._embedded['wp:featuredmedia'][0].media_details.sizes.large
+            .source_url;
+        return (
+          <li key={artist.id}>
+            <Artist
+              url={`/artist/${artist.slug}`}
+              title={artist.title.rendered}
+              artistImage={image}
+            />
+          </li>
+        );
+      });
     return <ul>{artistlistMarkup}</ul>;
   }
 }
