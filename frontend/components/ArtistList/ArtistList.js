7aa14bb57a920e230da4a67bb0467afeea9bfb8d
feeohnah <fiona@studiocartogram.com>
2018-09-29 15:19:14 -0400

    sorting artist list alphabetically

--- frontend/components/ArtistList/ArtistList.js
+++ frontend/components/ArtistList/ArtistList.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Link from '../Link';
 import {StyledArtistList, StyledArtistName, StyledArtistText} from './styles';
+import {compareLastNames} from '../../utilities';
 
 class ArtistList extends React.Component {
   render() {
     const {artists} = this.props;
-    const artistlistMarkup = artists.map((artist) => {
+    const sortedArtists = sortArtistByLastName(artists);
+
+    const artistlistMarkup = sortedArtists.map((artist) => {
       return (
         <StyledArtistName>
           <Link
@@ -35,3 +38,37 @@ class ArtistList extends React.Component {
   };
 }
 export default ArtistList;
+
+function sortArtistByLastName(artists) {
+  artists.sort((a, b) => {
+    console.log(a, b);
+    const aName = a.title.rendered.split(' ');
+    const bName = b.title.rendered.split(' ');
+    const aLastName = aName[aName.length - 1];
+    const bLastName = bName[bName.length - 1];
+
+    console.log(aName, aLastName);
+    console.log(bName, bLastName);
+
+    if (aLastName < bLastName) {
+      return -1;
+    }
+    if (aLastName > bLastName) {
+      return 1;
+    }
+    return 0;
+  });
+
+  return artists;
+}
+
+// //split the names as strings into arrays
+
+// // get the last names by selecting
+// // the last element in the name arrays
+// // using array.length - 1 since full names
+// // may also have a middle name or initial
+
+// // compare the names and return either
+// // a negative number, positive number
+// // or zero.
--- frontend/utilities/index.js
+++ frontend/utilities/index.js
@@ -10,6 +10,26 @@ export function getCurrentExhibition(exhibitions) {
   );
 }
 
+export function compareLastNames(a, b) {
+  //split the names as strings into arrays
+  var aName = a.split(' ');
+  var bName = b.split(' ');
+
+  // get the last names by selecting
+  // the last element in the name arrays
+  // using array.length - 1 since full names
+  // may also have a middle name or initial
+  var aLastName = aName[aName.length - 1];
+  var bLastName = bName[bName.length - 1];
+
+  // compare the names and return either
+  // a negative number, positive number
+  // or zero.
+  if (aLastName < bLastName) return -1;
+  if (aLastName > bLastName) return 1;
+  return 0;
+}
+
 export function getYearFromDateString(date) {
   return date.substr(-4);
 }
