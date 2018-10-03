0c54698b45c86d8c11bd680bb4c6a506caca7067
feeohnah <fiona@studiocartogram.com>
2018-10-03 19:54:21 -0400

    oops

--- frontend/components/ExhibitionList/ExhibitionList.js
+++ frontend/components/ExhibitionList/ExhibitionList.js
@@ -4,55 +4,54 @@ import Exhibition from '../Exhibition';
 import {getYearFromDateString, getFeaturedImage} from '../../utilities';
 
 function ExhibitionList({exhibitions, filters}) {
-  console.log(exhibitions);
-  // const exhibitionlistMarkup = exhibitions
-  //   .filter((exhibition) => {
-  //     const {artist, year} = filters;
-  //     if (!exhibition.acf.artist[0]) {
-  //       return false;
-  //     }
+  const exhibitionlistMarkup = exhibitions
+    .filter((exhibition) => {
+      const {artist, year} = filters;
+      if (!exhibition.acf.artist[0]) {
+        return false;
+      }
 
-  //     if (artist && year) {
-  //       return (
-  //         exhibition.acf.artist[0].post_title === artist &&
-  //         getYearFromDateString(exhibition.acf.start_date) === year
-  //       );
-  //     }
+      if (artist && year) {
+        return (
+          exhibition.acf.artist[0].post_title === artist &&
+          getYearFromDateString(exhibition.acf.start_date) === year
+        );
+      }
 
-  //     if (artist) {
-  //       return exhibition.acf.artist[0].post_title === artist;
-  //     }
+      if (artist) {
+        return exhibition.acf.artist[0].post_title === artist;
+      }
 
-  //     if (year) {
-  //       return getYearFromDateString(exhibition.acf.start_date) === year;
-  //     }
+      if (year) {
+        return getYearFromDateString(exhibition.acf.start_date) === year;
+      }
 
-  //     return true;
-  //   })
-  //   .map((exhibition) => {
-  //     const image = getFeaturedImage(exhibition);
+      return true;
+    })
+    .map((exhibition) => {
+      const image = getFeaturedImage(exhibition);
 
-  //     const artists = exhibition.acf.artist
-  //       ? exhibition.acf.artist.map(
-  //           (artist) => (artist ? artist.post_title : 'no artist'),
-  //         )
-  //       : [];
+      const artists = exhibition.acf.artist
+        ? exhibition.acf.artist.map(
+            (artist) => (artist ? artist.post_title : 'no artist'),
+          )
+        : [];
 
   return (
     <p>exhibitions</p>
-    // <StyledExhibitionLi key={exhibition.id}>
-    //   <Exhibition
-    //     slug={exhibition.slug}
-    //     url={`/exhibition/${exhibition.slug}`}
-    //     title={exhibition.title.rendered}
-    //     exhibitionImage={image}
-    //     artists={artists}
-    //     startdate={exhibition.acf.start_date}
-    //     enddate={exhibition.acf.end_date}
-    //   />
-    // </StyledExhibitionLi>
+    <StyledExhibitionLi key={exhibition.id}>
+      <Exhibition
+        slug={exhibition.slug}
+        url={`/exhibition/${exhibition.slug}`}
+        title={exhibition.title.rendered}
+        exhibitionImage={image}
+        artists={artists}
+        startdate={exhibition.acf.start_date}
+        enddate={exhibition.acf.end_date}
+      />
+    </StyledExhibitionLi>
   );
-  // });
+  });
 
   return (
     <React.Fragment>
