005e5cd05bd5da295a3aa2743669abfbd04b0eb4
feeohnah <fiona@studiocartogram.com>
2018-10-15 15:45:35 -0400

    fairs content

---
+++ frontend/components/NewsSingle/NewsSingle.js
@@ -0,0 +1,42 @@
+import Link from '../../components/Link';
+import PageMast from '../../components/PageMast';
+import PageText from '../../components/PageText';
+import Title from '../../components/Title';
+
+import {StyledArtistLink, StyledArtistLinkList, StyledContent} from './styles';
+
+function NewsSingle({url, slug, title, artists, content}) {
+  const artistListMarkup = artists.map((artist) => {
+    return (
+      <StyledArtistLink key={artist.post_name}>
+        <Link
+          variant="tertiary"
+          as={`/artist/${artist.post_name}`}
+          href={`/artist?slug=${artist.post_name}`}
+        >
+          {artist.post_title}
+        </Link>
+      </StyledArtistLink>
+    );
+  });
+
+  return (
+    <React.Fragment>
+      <PageMast>
+        <Title>
+          <div dangerouslySetInnerHTML={{__html: title}} />
+        </Title>
+      </PageMast>
+      <PageText>
+        <StyledContent>
+          <div dangerouslySetInnerHTML={{__html: content}} />
+        </StyledContent>
+        <StyledArtistLinkList>
+          More from {artistListMarkup}
+        </StyledArtistLinkList>
+      </PageText>
+    </React.Fragment>
+  );
+}
+
+export default NewsSingle;
