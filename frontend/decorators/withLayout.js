0c4ae084b80c5124733566f807e4cfff3fe110a0
feeohnah <fiona@studiocartogram.com>
2018-11-01 01:34:45 -0400

    removed gridwrapper

--- frontend/decorators/withLayout.js
+++ frontend/decorators/withLayout.js
@@ -7,7 +7,6 @@ import theme from '../styles/theme';
 import Layout from '../components/Layout';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import GridWrapper from '../components/GridWrapper';
 import Main from '../components/Main';
 import {endpoints} from '../config.js';
 
