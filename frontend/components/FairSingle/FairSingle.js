6f301e288147f71482f93eb271b9e81292bf33b9
feeohnah <fiona@studiocartogram.com>
2018-11-01 01:27:55 -0400

    link list for artists in fairs

--- frontend/components/FairSingle/FairSingle.js
+++ frontend/components/FairSingle/FairSingle.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import {config} from '../../config';
 import {withRouter} from 'next/router';
 import PageText from '../PageText';
 import PageMast from '../PageMast';
 import Thumbnail from '../Thumbnail';
 import Title from '../Title';
-import Fetcher from '../Fetcher';
-import Error from '../Error';
 import PageNav from '../PageNav';
 import Link from '../Link';
 import Modal from '../../components/Modal';
 import {getFileNameFromPath} from '../../utilities';
-
+import uniqBy from 'lodash/uniqBy';
+import flatten from 'lodash/flatten';
 class FairSingle extends React.Component {
   state = {
     currentSection: 'work',
@@ -19,51 +17,27 @@ class FairSingle extends React.Component {
   render() {
     const {currentSection} = this.state;
     const {title, works, content, startDate, endDate, slug} = this.props;
-
-    // const workArtists = works.map((work) => {
-    //   return work.work_artist[0];
-    // });
-
-    // const featuredArtists = work.reduce((acc, work) => {
-    //   const currentArtists = work.work_artist.map(
-    //     (artist) => artist && artist.post_title,
-    //   );
-    //   return [...acc, ...currentArtists];
-    // }, []);
-
-    // const newsArtists = news.reduce((acc, news) => {
-    //   const currentNewsArtists = news.acf.artist.map(
-    //     (artist) => artist && artist.post_title,
-    //   );
-
-    //   return [...acc, ...currentNewsArtists];
-    // }, []);
-
-    // const fairArtists = works.reduce((acc, work) => {
-    //   const currentFairArtists = work.work_artist.map(
-    //     (artist) => artist && artist.post_title,
-    //   );
-
-    //   return [...acc, ...currentFairArtists];
-    // }, []);
-
-    // console.log(featuredArtists);
-
-    // const workArtists = works.map((work) => {
-    //   return work.work_artist;
-    // });
-
-    // const workArtists = works.map((item) => {
-    //   const featuredArtists = item.work_artist.map(
-    //     (artist) => artist && artist.post_title,
-    //   );
-    //   return [...featuredArtists];
-    // }, []);
-
-    // console.log(featuredArtists);
-
-    // this gets me the first artist off the first Work
-    // console.log(works[0].work_artist[0].post_title);
+    const artists = flatten(
+      works.map((work) => {
+        return work.work_artist ? work.work_artist : [];
+      }),
+    );
+    const uniqueArtists = uniqBy(artists, 'post_name');
+    const artistListMarkup = uniqueArtists.map((artist, index) => {
+      console.log(index, uniqueArtists.length);
+      return (
+        <React.Fragment>
+          <Link
+            variant="tertiary"
+            as={`/artist/${artist.post_name}`}
+            href={`/artist?slug=${artist.post_name}`}
+          >
+            {artist.post_title}
+          </Link>
+          {index === uniqueArtists.length - 1 ? '' : ', '}
+        </React.Fragment>
+      );
+    });
 
     function sectionMarkup(currentSection) {
       switch (currentSection) {
@@ -78,14 +52,20 @@ class FairSingle extends React.Component {
             return null;
           }
           const workImageMarkup = works.map((work) => {
-            const id = getFileNameFromPath(work.work_image);
+            const id = getFileNameFromPath(
+              work.work_image.sizes.img_large ||
+                work.work_image.sizes.img_medium,
+            );
             const href = `/fair?slug=${slug}&id=${id}`;
             const as = `/fair/${slug}?id=${id}`;
             return (
               <Thumbnail
                 url={work.work_image}
                 title={work.work_title}
-                image={work.work_image}
+                image={
+                  work.work_image.sizes.img_large ||
+                  work.work_image.sizes.img_medium
+                }
                 artist={work.work_artist}
                 key={id}
                 id={id}
@@ -102,16 +82,33 @@ class FairSingle extends React.Component {
     return (
       <React.Fragment>
         <Modal current collection={works} />
+        <div>
+          <Link as="/fairs" href="/fairs">
+            All fairs
+          </Link>
+        </div>
         <PageMast>
           <Title>
             <div dangerouslySetInnerHTML={{__html: title}} />
           </Title>
           <p>
             {startDate} - {endDate}
+            <br />
+            Featuring: {artistListMarkup}
           </p>
           <PageNav>
-            <Link onClick={this.handleSectionChange('work')}>Work</Link>
-            <Link onClick={this.handleSectionChange('about')}>About</Link>
+            <Link
+              current={currentSection === 'work'}
+              onClick={this.handleSectionChange('work')}
+            >
+              Work
+            </Link>
+            <Link
+              current={currentSection === 'about'}
+              onClick={this.handleSectionChange('about')}
+            >
+              About
+            </Link>
           </PageNav>
         </PageMast>
         {subSectionMarkup}
