5eab24de0a68d741973f390f340cc0e6a8864ac4
feeohnah <fiona@studiocartogram.com>
2018-11-01 01:20:50 -0400

    typo

--- frontend/components/CurrentExhibition/CurrentExhibition.js
+++ frontend/components/CurrentExhibition/CurrentExhibition.js
@@ -5,7 +5,7 @@ import {StyledImage, StyledImageContainer} from './styles';
 function CurrentExhibition({url, slug, image, title}) {
   return (
     <StyledImageContainer>
-      <Link as={url} href={`/exhibitions?slug=${slug}`}>
+      <Link as={url} href={`/exhibition?slug=${slug}`}>
         <StyledImage key={title} alt={title} src={image} />
       </Link>
     </StyledImageContainer>
