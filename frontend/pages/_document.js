3413f878f499dfdeefee4243d0f25fbd3ef21f63
feeohnah <fiona@studiocartogram.com>
2018-11-10 14:28:49 -0500

    updated document

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,31 +1,20 @@
-import NextDocument, { Head, Main, NextScript } from "next/document";
-import { ServerStyleSheet } from "styled-components";
-import "../styles/global";
+import Document from 'next/document';
+import {ServerStyleSheet} from 'styled-components';
 
-export default class Document extends NextDocument {
-  static getInitialProps({ renderPage }) {
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
-    const page = renderPage(App => props =>
-      sheet.collectStyles(<App {...props} />)
-    );
-    const styleTags = sheet.getStyleElement();
-    return { ...page, styleTags };
-  }
 
-  render() {
-    return (
-      <html>
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta charSet="utf-8" />
-          <title>Daniel Faria Gallery, Toronto Canada</title>
-          {this.props.styleTags}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </html>
-    );
+    const originalRenderPage = ctx.renderPage;
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: [...initialProps.styles, ...sheet.getStyleElement()],
+    };
   }
 }
