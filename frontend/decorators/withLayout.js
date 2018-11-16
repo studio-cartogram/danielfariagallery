b81d5c0c0463008e1a43d5306b0add30cc0eeddd
feeohnah <fiona@studiocartogram.com>
2018-11-16 00:23:46 -0500

    favicon

--- frontend/decorators/withLayout.js
+++ frontend/decorators/withLayout.js
@@ -96,6 +96,31 @@ function App({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
         <title>Daniel Faria Gallery, Toronto Canada</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/static/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/static/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/static/images/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/static/images/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/static/images/safari-pinned-tab.svg"
+          color="#003468"
+        />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <ThemeProvider theme={theme}>
         <Layout>
